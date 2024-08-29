5cabbcfdf29be36a0e217fce6b66369e4ca18a19
manoj kaushik <work.manojkaushik@gmail.com>
2024-08-29 21:23:24 +0530

    google adsense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,13 +17,13 @@ export default function Home() {
       {/* Google AdSense Ad Slot */}
       <ins
         className="adsbygoogle"
-        style={{ display: "block" }}
+        style={{ display: "block", backgroundColor:"orchid" }}
         data-ad-client="ca-pub-5586423585632688"
         data-ad-slot="5043688208"
         data-ad-format="auto"
         data-full-width-responsive="true"
       ></ins>
-     <HomePage/>
+     {/* <HomePage/> */}
   
     </>
 
