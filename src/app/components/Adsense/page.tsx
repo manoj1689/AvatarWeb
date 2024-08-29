6432461efc61d80140c438c629f1c4b809dc946d
manoj kaushik <work.manojkaushik@gmail.com>
2024-08-29 13:43:24 +0530

    google adsense

--- src/app/components/Adsense/page.tsx
+++ src/app/components/Adsense/page.tsx
@@ -9,17 +9,12 @@ const AdSence = () => {
     <Script
       id="adsense-script"
       strategy="afterInteractive"
-     src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5586423585632688"
+      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5586423585632688"
       async
       crossOrigin="anonymous"
       onLoad={() => {
         console.log('Adsense script loaded successfully');
-        try {
-          // Initialize Google Ads
-          (window.adsbygoogle = window.adsbygoogle || []).push({});
-        } catch (e) {
-          console.error('Adsbygoogle initialization error:', e);
-        }
+      
       }}
       onError={(e) => console.error('Adsense script failed to load', e)}
     />
