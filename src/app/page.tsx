ccab2372b2ffdbfbd05bab7f3289e2bdf228a627
manoj kaushik <work.manojkaushik@gmail.com>
2024-09-05 09:53:40 +0530

    Code Update Landing Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,14 +31,14 @@ export default function Home() {
       </div>
       
       {/* Google AdSense Ad Slot */}
-      {/* <ins
+      <ins
         className="adsbygoogle"
         style={{ display: "block", backgroundColor: "orange" }}
         data-ad-client="ca-pub-5586423585632688"
         data-ad-slot="5043688208"
         data-ad-format="auto"
         data-full-width-responsive="true"
-      ></ins> */}
+      ></ins>
     </div>
   );
 }
